1229375088fea85f2cbed345b55f3bc97ba5c8b8
abhishekgururani <gururani.abhishek@gmail.com>
2021-08-06 14:43:45 +0530

    done with implementing : distict data

---
+++ app/src/main/java/org/example/uttarakhandcovidtracker/District.kt
@@ -0,0 +1,9 @@
+package org.example.uttarakhandcovidtracker
+
+data class District(
+    val district_name : String,
+    val active : String,
+    val confirmed : String,
+    val deceased : String,
+    val recovered : String
+)
---
+++ app/src/main/java/org/example/uttarakhandcovidtracker/DistrictDataActivity.kt
@@ -0,0 +1,56 @@
+package org.example.uttarakhandcovidtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+
+class DistrictDataActivity : AppCompatActivity() {
+    private lateinit var mAdapter : DistrictDataAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_district_data)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val items = fetchData()
+        mAdapter = DistrictDataAdapter()
+        recyclerView.adapter = mAdapter
+    }
+
+    private fun fetchData() {
+        val url = "https://api.covid19india.org/v2/state_district_wise.json"
+        val jsonArrayRequest = JsonArrayRequest (
+        Request.Method.GET, url, null,
+            {
+                val districtArray = ArrayList<District>()
+
+                val jsonObject = it.getJSONObject(35)
+                val jsonArray_districts  = jsonObject.getJSONArray("districtData")
+
+                for(i in 0 until jsonArray_districts.length()) {
+                    val jsonObject_district = jsonArray_districts.getJSONObject(i)
+                    val district = District(
+                        jsonObject_district.getString("district"),
+                        jsonObject_district.getString("active"),
+                        jsonObject_district.getString("confirmed"),
+                        jsonObject_district.getString("deceased"),
+                        jsonObject_district.getString("recovered")
+                    )
+                    districtArray.add(district)
+                }
+                mAdapter.updateDistricts(districtArray)
+
+            },
+            {
+                Toast.makeText(this, "error in the hood boys!", Toast.LENGTH_SHORT).show()
+            }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest)
+
+    }
+}
